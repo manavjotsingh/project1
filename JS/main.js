7e020da3e33a17733c0f8fa5f776f621428e4f8e
Manav Singh <manavjot13@gmail.com>
2019-10-10 21:34:45 -0700

    added email submit alert but not email fail, added working cart counter but didnt use a loop, some other small changes

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,68 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+document.addEventListener("DOMContentLoaded", function () { //COMPLETE WRAP BEGINNING
+
+
+// getting the message for submitting the email.
+  const messageForm = document.getElementById('emailform');
+
+const eMail = document.getElementById('youremail');
+
+messageForm.addEventListener('submit', function(event) {
+event.preventDefault();
+
+const submittedEmail = eMail.nodeValue;
+
+if (submittedEmail !== "") {
+  alert ("Thanks for subscribing!");
+}
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+let clicks = 0;
+
+
+const clickCount = document.getElementById ('clickcount');
+
+clickCount.innerHTML = "";
+
+const cartClick = document.getElementsByClassName ('tocart')[0];
+cartClick.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+
+const cartClick1 = document.getElementsByClassName ('tocart')[1];
+cartClick1.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+
+const cartClick2 = document.getElementsByClassName ('tocart')[2];
+cartClick2.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+const cartClick3 = document.getElementsByClassName ('tocart')[3];
+cartClick3.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+const cartClick4 = document.getElementsByClassName ('tocart')[4];
+cartClick4.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+const cartClick5 = document.getElementsByClassName ('tocart')[5];
+cartClick5.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+const cartClick6 = document.getElementsByClassName ('tocart')[6];
+cartClick6.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+const cartClick7 = document.getElementsByClassName ('tocart')[7];
+cartClick7.addEventListener('click', function evovle(){
+  clicks++;
+  clickCount.innerHTML = clicks;
+})
+}); //COMPLETE WRAP END
